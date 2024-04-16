2644bc7dcca1ac76b74f3784d12d366c86ce8624
Théo BARCELO <theo.barcelo@etu.univ-amu.fr>
2024-04-16 09:37:36 +0200

    Ajout de l'exercice 4

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,11 +1,17 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Palette extends Application {
@@ -25,7 +31,65 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Création d'un conteneur VBox avec ses éléments centrés
+        this.root = new BorderPane();
+
+        // Création de la scene
+        Scene scene = new Scene( root );
+
+        this.vert = new Button("Vert");
+        this.rouge = new Button("Rouge");
+        this.bleu = new Button("Bleu");
+        HBox.setMargin( vert, new Insets(10.0d, 5.0d, 10.0d, 5.0d));
+        HBox.setMargin( rouge, new Insets(10.0d, 5.0d, 10.0d, 5.0d));
+        HBox.setMargin( bleu, new Insets(10.0d, 5.0d, 10.0d, 5.0d));
+
+        vert.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> handleVert(actionEvent) );
+        rouge.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> handleRouge(actionEvent) );
+        bleu.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> handleBleu(actionEvent) );
+
+        this.panneau = new Pane();
+
+        this.bas = new HBox();
+        bas.setAlignment( Pos.CENTER );
+        bas.getChildren().addAll(vert, rouge, bleu);
+
+        HBox haut = new HBox();
+
+        this.label = new Label("Couleur choisie");
+        label.setFont( Font.font("Courier", FontWeight.NORMAL, 18) );
+        haut.setAlignment(Pos.CENTER);
+        haut.getChildren().add(label);
+
+        root.setTop(haut);
+        root.setCenter(panneau);
+        root.setBottom(bas);
+
+        // Ajout de la scene à la fenêtre
+        primaryStage.setScene( scene );
+
+        primaryStage.setTitle("Choisi la couleur !!");
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(200);
         primaryStage.show();
+    }
+
+    private void handleVert(Event event) {
+        nbVert++;
+        panneau.setStyle("-fx-background-color: green");
+        label.setText("Vert choisi " + nbVert + " fois");
+    }
+
+    private void handleRouge(Event event) {
+        nbRouge++;
+        panneau.setStyle("-fx-background-color: red");
+        label.setText("Rouge choisi " + nbRouge + " fois");
+    }
+
+    private void handleBleu(Event event) {
+        nbBleu++;
+        panneau.setStyle("-fx-background-color: blue");
+        label.setText("Bleu choisi " + nbBleu + " fois");
     }
 }
 
