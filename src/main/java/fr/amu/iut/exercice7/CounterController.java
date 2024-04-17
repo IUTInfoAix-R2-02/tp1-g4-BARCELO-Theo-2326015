0f914f5e86e90f3a2ac34e68639913bef3087298
Théo BARCELO <theo.barcelo@etu.univ-amu.fr>
2024-04-17 11:54:08 +0200

    Exercice 7 et 8 TP3

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,22 +1,32 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
+    @FXML
+    private Label counterLabel;
 
     int counter = 0;
 
     public void increment() {
+        counter++;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     public void decrement() {
+        counter--;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initializing CounterController...");
-   }
+        counterLabel.setText(String.valueOf(counter));
+    }
 }
--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,34 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Window;
 
 public class LoginControl extends GridPane {
 
+    @FXML
+    Button okButton;
+    @FXML
+    Button cancelButton;
+
+    @FXML
+    TextField userid;
+    @FXML
+    PasswordField pwd;
+
+    @FXML
     private void okClicked() {
+        System.out.println(userid.getText());
+        System.out.println("*".repeat(pwd.getText().length()));
     }
 
+    @FXML
     private void cancelClicked() {
+        userid.setText("");
+        pwd.setText("");
     }
 }
